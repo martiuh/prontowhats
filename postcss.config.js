cf63d41e8d4ea19aa6506851800dd7b0cf8c79de
Tona González <martiuh@gmail.com>
2021-04-22 09:54:03 -0500

    fix(postcss.config): purge dist/index.html

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['public/index.html'],
+  content: ['dist/index.html'],
   // This is the function used to extract class names from your templates
   defaultExtractor: (content) => {
     // Capture as liberally as possible, including things like `h-(screen-1.5)`
