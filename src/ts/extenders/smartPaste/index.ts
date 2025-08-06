1f1d26b2aa1f0f56b61930139d315b7232ec7fdc
Tona González <martiuh@gmail.com>
2025-08-06 12:27:28 -0600

    chore: remove unused files

--- src/ts/extenders/smartPaste/index.ts
+++
@@ -1,1 +0,0 @@
-import { extendTelField } from './extendTelField';
--- src/ts/extenders/smartPaste/tests/utils.ts
+++
@@ -1,9 +0,0 @@
-export function createPasteEvent(html: string) {
-  const text = html.replace('<[^>]*>', '');
-  return {
-    clipboardData: {
-      types: ['text/plain', 'text/html'],
-      getData: (type: string) => (type === 'text/plain' ? text : html),
-    },
-  };
-}
